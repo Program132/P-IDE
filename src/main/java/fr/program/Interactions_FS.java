bf6a0808deb30c61f560cca2c03bf62023154c8f
Program132 <clement.gauthier132@orange.fr>
2023-07-09 21:52:03 +0200

    Process + Opti

--- src/main/java/fr/program/Interactions_FS.java
+++ src/main/java/fr/program/Interactions_FS.java
@@ -1,12 +1,14 @@
 package fr.program;
 
 import javafx.application.Platform;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.scene.control.TextArea;
 
 import java.io.*;
 
 public class Interactions_FS {
-    private static String mode;
+    private static String mode = "N/A";
     private static String getMode() {
         return mode;
     }
@@ -14,43 +16,29 @@ public class Interactions_FS {
         mode = m;
     }
 
-    public static void execute(String content, TextArea zone) throws IOException {
+    public static void execute(String content, TextArea output_zone) throws IOException {
         // FORMAT : ProcessBuilder processBuilder = new ProcessBuilder("chemin_vers_executable", "argument1", "argument2");
 
         if (getMode().equalsIgnoreCase("fpl")) {
             writeInFile("fastscript/code/fpl.fpl", content);
 
-
+            // Running FPL V3 Parser
         } else if (getMode().equalsIgnoreCase("java")) {
             writeInFile("fastscript/code/java.java", content);
 
-
+            executeCodeAndSeeOutput(output_zone, "bin/jre-1.8/bin/java.exe", "Java", "fastscript/code/java.java");
         } else if (getMode().equalsIgnoreCase("cpp")) {
             writeInFile("fastscript/code/cpp.cpp", content);
 
-
+            // Running cmake and g++
         } else if (getMode().equalsIgnoreCase("lua")) {
             writeInFile("fastscript/code/lua.lua", content);
 
-            ProcessBuilder processBuilder = new ProcessBuilder("bin/lua-5.4.2/lua54.exe");
-            processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
-            Process process = processBuilder.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            String all_lines = "";
-            while ((line = reader.readLine()) != null) {
-                all_lines += line + "\n";
-            }
-            reader.close();
-
-            String finalAll_lines = all_lines;
-            Platform.runLater(() -> {
-                zone.setText(finalAll_lines);
-            });
+            executeCodeAndSeeOutput(output_zone, "bin/lua-5.4.2/lua54.exe", "Lua", "fastscript/code/lua.lua");
         } else if (getMode().equalsIgnoreCase("py")) {
             writeInFile("fastscript/code/py.py", content);
 
-
+            executeCodeAndSeeOutput(output_zone, "bin/Python311/python.exe", "Python", "fastscript/code/py.py");
         }
     }
 
@@ -65,5 +53,39 @@ public class Interactions_FS {
                 //
             }
         }
+    }
+
+    private static String openShell(String pathApplication, String lang, String arg) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(pathApplication, arg);
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        String all_lines = "Running in " + lang + ": \n";
+        while ((line = reader.readLine()) != null) {
+            all_lines += line + "\n";
+        }
+        reader.close();
+
+        return all_lines;
+    }
+
+    private static void executeCodeAndSeeOutput(TextArea output_zone, String pathApp, String lang, String arg) {
+        Service<String> service = new Service<String>() {
+            @Override
+            protected Task<String> createTask() {
+                return new Task<String>() {
+                    @Override
+                    protected String call() throws Exception {
+                        return openShell(pathApp, lang, arg);
+                    }
+                };
+            }
+        };
+        service.setOnSucceeded(event -> {
+            String result = service.getValue();
+            output_zone.setText(result);
+        });
+        service.start();
     }
 }
