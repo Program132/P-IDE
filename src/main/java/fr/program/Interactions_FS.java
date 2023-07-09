67261eff3c3a189865425db2dfa802d8bbb590ec
Program132 <clement.gauthier132@orange.fr>
2023-07-09 19:51:50 +0200

    Interactions Buttons (Fast Script UI)

---
+++ src/main/java/fr/program/Interactions_FS.java
@@ -0,0 +1,12 @@
+package fr.program;
+
+public class Interactions_FS {
+    private static String mode;
+    public static String getMode() {
+        return mode;
+    }
+
+    public static void setMode(String m) {
+        mode = m;
+    }
+}
