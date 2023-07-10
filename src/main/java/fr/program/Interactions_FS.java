815e2df535e724b31b29ff0a3db57eb4049d2526
Program132 <clement.gauthier132@orange.fr>
2023-07-10 16:33:30 +0200

    Unused lib/funcs

--- src/main/java/fr/program/Interactions_FS.java
+++ src/main/java/fr/program/Interactions_FS.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
-import java.util.stream.Stream;
 
 public class Interactions_FS {
     private static String mode = "N/A";
